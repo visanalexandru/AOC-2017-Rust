cc0194c4f005d5ddad846540cda09964696dc39c
visan <alexandruvisan44@yahoo.com>
2022-02-08 15:33:46 +0200

    formated day 7.

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -78,75 +78,80 @@ fn solve1(map: &HashMap<String, Program>) -> &Program {
 
 fn solve2(root: &Program, map: &HashMap<String, Program>) -> u32 {
     let mut weights = HashMap::new();
-    let mut invalid= HashMap::new();
+    let mut invalid = HashMap::new();
 
-    dfs1(root, map, &mut weights,&mut invalid);
+    dfs1(root, map, &mut weights, &mut invalid);
 
-    let mut now=root;
-    let mut found=true;
+    let mut now = root;
+    let mut found = true;
 
-    while found{
-        found=false;
+    while found {
+        found = false;
 
-        for child in &now.children{
-            if *invalid.get(child).unwrap(){
-                now=map.get(child).unwrap();
-                found=true;
+        for child in &now.children {
+            if *invalid.get(child).unwrap() {
+                now = map.get(child).unwrap();
+                found = true;
             }
         }
     }
 
-    let mut majority=weights.get(&now.children[0]).unwrap(); 
-    let mut count=0;
+    let mut majority = weights.get(&now.children[0]).unwrap();
+    let mut count = 0;
 
-    for child in &now.children{
-        let here=weights.get(child).unwrap();
-        if here==majority{
-            count+=1;
-        }
-    }    
-
-    if count==1{
-        majority=weights.get(&now.children[1]).unwrap();
-    }
-    
-    let mut change=&now.children[0];
-    for child in &now.children{
-        if weights.get(child).unwrap() != majority{
-            change=&child;
+    for child in &now.children {
+        let here = weights.get(child).unwrap();
+        if here == majority {
+            count += 1;
         }
     }
-    let weight_to_change=weights.get(change).unwrap();
-    let current=map.get(change).unwrap().weight;
 
-    current+majority-weight_to_change
+    if count == 1 {
+        majority = weights.get(&now.children[1]).unwrap();
+    }
+
+    let mut change = &now.children[0];
+    for child in &now.children {
+        if weights.get(child).unwrap() != majority {
+            change = &child;
+        }
+    }
+    let weight_to_change = weights.get(change).unwrap();
+    let current = map.get(change).unwrap().weight;
+
+    current + majority - weight_to_change
 }
 
 //creates the weights table
-fn dfs1(current: &Program, map: &HashMap<String, Program>, weights: &mut HashMap<String, u32>,invalid:&mut HashMap<String,bool>) {
+fn dfs1(
+    current: &Program,
+    map: &HashMap<String, Program>,
+    weights: &mut HashMap<String, u32>,
+    invalid: &mut HashMap<String, bool>,
+) {
     let mut tower_weight = current.weight;
-    let mut child_weights=Vec::new();
-    let mut incorrect=false;
+    let mut child_weights = Vec::new();
+    let mut incorrect = false;
 
     for child in &current.children {
         let next_program = map.get(child).unwrap();
-        dfs1(next_program, map, weights,invalid);
-        let child_weight=*weights.get(child).unwrap();
+        dfs1(next_program, map, weights, invalid);
+        let child_weight = *weights.get(child).unwrap();
         tower_weight += child_weight;
 
-        let child_correct=*invalid.get(child).unwrap();
-        incorrect|=child_correct;
+        let child_correct = *invalid.get(child).unwrap();
+        incorrect |= child_correct;
 
         child_weights.push(child_weight);
     }
 
-    for &w in &child_weights{
-        if w != child_weights[0]{
-            incorrect=true;
+    for &w in &child_weights {
+        if w != child_weights[0] {
+            incorrect = true;
             break;
         }
     }
-    
+
     weights.insert(current.name.clone(), tower_weight);
     invalid.insert(current.name.clone(), incorrect);
 }
@@ -156,5 +161,5 @@ fn main() {
     let map = parse_lines(&lines);
     let root = solve1(&map);
     println!("{}", root.name);
-    println!("{}",solve2(&root, &map));
+    println!("{}", solve2(&root, &map));
 }
